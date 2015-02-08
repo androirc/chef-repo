6bbe646b3c74654ec517defaf696684f14e60f9c
Sébastien Brochet <blinkseb@madalynn.eu>
2015-02-08 17:45:06 +0100

    Update default role

--- roles/default.rb
+++ roles/default.rb
@@ -1,3 +1,3 @@
 name "default"
 description "Default configuration of a node"
-run_list "recipe[chef-vault]", "recipe[madalynn::users]"
+run_list "recipe[chef-client]", "recipe[chef-vault]", "recipe[madalynn::users]"

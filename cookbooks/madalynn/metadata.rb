1fcae96092b371014e96dd31e8ae9c9ab86a4707
Sébastien Brochet <blinkseb@madalynn.eu>
2015-02-09 13:10:01 +0100

    Update metadata

--- cookbooks/madalynn/metadata.rb
+++ cookbooks/madalynn/metadata.rb
@@ -4,7 +4,8 @@ maintainer_email 'blinkseb@madalynn.eu'
 license          'All rights reserved'
 description      'Installs/Configures madalynn'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.0'
+version          '0.1.1'
 
 depends 'user'
+depends 'sudo'
 depends 'chef-vault'

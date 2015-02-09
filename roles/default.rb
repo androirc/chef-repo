6721c234c54021bbf8a28e06396f8022e660b479
Sébastien Brochet <blinkseb@madalynn.eu>
2015-02-09 21:42:04 +0100

    Add unattended-upgrades to run list

--- roles/default.rb
+++ roles/default.rb
@@ -1,6 +1,6 @@
 name "default"
 description "Default configuration of a node"
-run_list "recipe[chef-client::config]", "recipe[chef-client::service]", "recipe[chef-vault]", "recipe[madalynn::users]"
+run_list "recipe[chef-client::config]", "recipe[chef-client::service]", "recipe[chef-vault]", "recipe[madalynn::users]", "recipe[unattended-upgrades]"
 default_attributes(
   "authorization" => {
       "sudo" => {

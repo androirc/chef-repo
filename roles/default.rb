8eeeb1521d3670a17952c931f6587c46d10fe9c4
Sébastien Brochet <blinkseb@madalynn.eu>
2015-02-09 13:13:15 +0100

    Add default sudo attributes

--- roles/default.rb
+++ roles/default.rb
@@ -1,3 +1,18 @@
 name "default"
 description "Default configuration of a node"
 run_list "recipe[chef-client]", "recipe[chef-vault]", "recipe[madalynn::users]"
+default_attributes(
+  "authorization" => {
+      "sudo" => {
+            "groups" => ["admin", "sudo"],
+            "users" => ["root"],
+            "passwordless" => false,
+            "include_sudoers_d" => true,
+            "sudoers_defaults" => [
+              'env_reset',
+              'mail_badpass',
+              'secure_path="/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin"'
+            ]
+          }
+    }
+)

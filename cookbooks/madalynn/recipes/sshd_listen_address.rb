240bc2387d6eef2752aa56b06b0dd524a21b7ac2
Sébastien Brochet <blinkseb@madalynn.eu>
2015-02-09 13:32:01 +0100

    Fix foodcritic FC001

--- cookbooks/madalynn/recipes/sshd_listen_address.rb
+++ cookbooks/madalynn/recipes/sshd_listen_address.rb
@@ -9,7 +9,7 @@
 
 # Change sshd ListenAddress to listen on private ip (eth1)
 
-interface = (node[:network][:interfaces].key?(:eth1) && "eth1") || "eth0"
+interface = (node['network']['interfaces'].key?('eth1') && 'eth1') || 'eth0'
 
-ip = node[:network][:interfaces][interface][:addresses].detect{ |k, v| v[:family] == "inet" }.first
+ip = node['network']['interfaces'][interface]['addresses'].detect{ |k, v| v['family'] == 'inet' }.first
 node.override['sshd']['sshd_config']['ListenAddress'] = ip
